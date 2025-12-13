f2c6c2f63e7b4e7171ee4ebe6b081a750a9cd835
leevicode <leevii.leino@gmail.com>
2025-12-13 21:58:14 +0200

    WIP: implement InputManager

---
+++ src/input.c
@@ -0,0 +1,14 @@
+#include "input.h"
+#define NUM_INPUTS 5
+InputDevice inputs[NUM_INPUTS];
+
+InputManager *getManager() {
+  {
+  }
+  for (int i = 1; i < NUM_INPUTS; i++) {
+  }
+  /**
+   * doing important stuff
+   */
+  return &getManager;
+}
